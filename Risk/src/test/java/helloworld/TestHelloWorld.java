fe122634c3d2a8622cf978070e5f4f3deb3d294b
AdarshPawar <adars@AdarshAkash>
2023-09-21 10:32:59 -0400

    Integration with Maven along with dependency - JUnit

---
+++ Risk/src/test/java/helloworld/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testHelloWorld() {
+		System.out.println("Successfully passed test\n");
+	}
+
+}
