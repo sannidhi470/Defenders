138f3731057e66ab195c07d1c416acab0e9e3416
Sannidhi Shetty <sannidhishetty9@gmail.com>
2023-09-23 22:54:34 -0400

    Update Country model to include d_neighbours parameter and get_nameFromId() method

--- Risk/src/main/java/Models/Country.java
+++ Risk/src/main/java/Models/Country.java
@@ -1,19 +1,46 @@
 package Models;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Country {
 
 	private int d_countryId;
 	private String d_countryName;
 	private int d_continentId;
+	private ArrayList<Integer> d_neighbours;
+	public Country()
+	{
+		
+	}
 	
-	public Country(int d_countryId, String d_countryName, int d_continentId) {
+	public Country(int d_countryId, String d_countryName, int d_continentId,ArrayList<Integer> d_neighbours) {
 		this.d_countryId = d_countryId;
 		this.d_countryName = d_countryName;
 		this.d_continentId = d_continentId;
+		this.d_neighbours=d_neighbours;
+	}
+
+	public ArrayList<Integer> getD_neighbours() {
+		return d_neighbours;
+		
+	}
+
+
+	public void setD_neighbours(int l_countryId,HashMap<Integer,ArrayList<Integer>> l_neighbouringCountries) {
+		this.d_neighbours=l_neighbouringCountries.get(l_countryId);
 	}
 
 	public int getD_countryId() {
 		return d_countryId;
+	}
+
+	public int getD_continentId() {
+		return d_continentId;
+	}
+
+	public void setD_continentId(int d_continentId) {
+		this.d_continentId = d_continentId;
 	}
 
 	public void setD_countryId(int d_countryId) {
@@ -26,5 +53,19 @@ public class Country {
 
 	public void setD_countryName(String d_countryName) {
 		this.d_countryName = d_countryName;
+	}
+	public String get_nameFromId(ArrayList<Country> p_countryList,int p_countryId)  
+	{
+		//need to add exception
+		for(Country i:p_countryList)
+		{
+			if(i.getD_countryId()==p_countryId)
+			{
+				return i.getD_countryName();
+			}
+			
+		}
+		return " ";
+		
 	}
 }
