9c1a380af325971b7cf4f7f352cd5c4853ae8866
daiviksaharan <daiviksaharan@daiviks-air.home>
2023-09-23 23:01:35 -0400

    Updated Continent class to add d_getCountryFromContinentId,d_countries

--- Risk/src/main/java/Models/Continent.java
+++ Risk/src/main/java/Models/Continent.java
@@ -8,16 +8,33 @@ public class Continent {
 	private int d_continentArmyValue;
 	private List<Country> d_countries;
 	
+	public Continent()
+	{
+		
+	}
+	
 	public Continent(int d_continentId, String d_continentName, int d_continentArmyValue, List<Country> d_countries) {
 		super();
 		this.d_continentId = d_continentId;
 		this.d_continentName = d_continentName;
 		this.d_continentArmyValue = d_continentArmyValue;
-		this.d_countries = new ArrayList<>();
+		this.d_countries = d_countries;
 	}
 
 	public int getD_continentId() {
 		return d_continentId;
+	}
+
+	public List<Country> getD_countries() {
+		return d_countries;
+	}
+
+	public void setD_countries(List<Country> d_countries) {
+		this.d_countries = d_countries;
+	}
+
+	public int getD_continentArmyValue() {
+		return d_continentArmyValue;
 	}
 
 	public void setD_continentId(int d_continentId) {
@@ -38,5 +55,16 @@ public class Continent {
 
 	public void setD_continentArmyValue(int d_continentArmyValue) {
 		this.d_continentArmyValue = d_continentArmyValue;
+	}
+	
+	public ArrayList<Country> d_getCountryFromContinentId(int p_continentId,ArrayList<Country> p_countryList)
+	{
+		ArrayList<Country> l_countries=new ArrayList<Country>();
+		for(Country i:p_countryList)
+		{
+			if(i.getD_continentId()==p_continentId)
+				l_countries.add(i);
+		}
+		return l_countries;
 	}
 }
