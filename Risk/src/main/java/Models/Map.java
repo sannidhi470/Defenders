1e566c6ede9b18edd110829c99fad046d27736f8
Sannidhi Shetty <sannidhishetty9@gmail.com>
2023-09-25 15:57:52 -0400

    Added Map model

---
+++ Risk/src/main/java/Models/Map.java
@@ -0,0 +1,26 @@
+package Models;
+
+public class Map {
+
+	private String d_mapFileName;
+	private boolean d_mapExist;
+	
+	public Map()
+	{
+		this.d_mapExist=false;
+	}
+	public String getD_mapFileName() {
+		return d_mapFileName;
+	}
+	public void setD_mapFileName(String d_mapFileName) {
+		this.d_mapFileName = d_mapFileName;
+	}
+	public boolean isD_mapExist() {
+		return d_mapExist;
+	}
+	public void setD_mapExist(boolean d_mapExist) {
+		this.d_mapExist = d_mapExist;
+	}
+	
+	
+}
