94b54f6d0f2e27af2a5353869324040fd6ca771f
daiviksaharan <daiviksaharan@daiviks-air.wireless.concordia.ca>
2023-09-25 16:01:03 -0400

    Created MapCheck class to validate map in the services.

---
+++ Risk/src/main/java/Services/MapCheck.java
@@ -0,0 +1,25 @@
+package Services;
+import java.io.File;
+
+import Utilities.*;
+public class MapCheck {
+
+	public static boolean validateMap(String p_enteredFileName,String p_pathName)
+	{
+		p_enteredFileName=p_enteredFileName+".map";
+		
+		File l_resourcePath=new File(p_pathName);
+		File l_fileList[]=l_resourcePath.listFiles();
+		for(File l_file:l_fileList)
+		{
+			if(p_enteredFileName.compareTo(l_file.getName())==0)
+				return true;
+		}
+		
+		
+		return false;
+	}
+	
+	
+
+}
