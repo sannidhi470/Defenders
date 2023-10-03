417a1087d2fd59eb764029fc796fc21571d4f3db
AdarshPawar <adars@AdarshAkash>
2023-10-03 00:22:34 -0400

    Added exception handling for MapCreator

--- Risk/src/main/java/Tools/MapCreater.java
+++ Risk/src/main/java/Tools/MapCreater.java
@@ -2,24 +2,27 @@ package Tools;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-
-import Models.Continent;
 
 public class MapCreater {
 
-	public static void createMap(String p_fileName,String p_pathName) throws IOException
+	public static void createMap(String p_fileName,String p_pathName) 
 	{
 		
-		Scanner l_input=new Scanner(System.in);
 		File directory = new File(p_pathName);
 		File f = new File(directory,p_fileName+".map");
-		boolean success = f.createNewFile() ;
+		boolean success;
 		//Need to add exception for file not found
-		System.out.println(success);
-		//File l_openedFile
-		
-		
+		try {
+			success = f.createNewFile();
+			if(success) 
+				System.out.println("File Created successfully");
+			else
+				System.out.println("Unable to create new file");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Unable to create a new file");
+			e.printStackTrace();
+		}
 	}
 }
