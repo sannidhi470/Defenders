8c33ee278e7849e1e77c153ffa9ab9e210d91ea2
daiviksaharan <daiviksaharan@daiviks-air.hitronhub.home>
2023-10-02 22:59:00 -0400

    Refactored validateMap of MapCheck

--- Risk/src/main/java/Tools/MapCheck.java
+++ Risk/src/main/java/Tools/MapCheck.java
@@ -1,23 +1,14 @@
 package Tools;
 import java.io.File;
-public class MapCheck {
-
+public class MapCheck 
+{
 	public static boolean validateMap(String p_enteredFileName,String p_pathName)
 	{
-		p_enteredFileName=p_enteredFileName+".map";
-		
+		p_enteredFileName=p_enteredFileName+".map";		
 		File l_resourcePath=new File(p_pathName);
 		File l_fileList[]=l_resourcePath.listFiles();
 		for(File l_file:l_fileList)
-		{
-			if(p_enteredFileName.compareTo(l_file.getName())==0)
-				return true;
-		}
-		
-		
+			if(p_enteredFileName.compareTo(l_file.getName())==0) return true;
 		return false;
 	}
-	
-	
-
-}
+} 
