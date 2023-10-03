d375050014d31673381019487bd62744e2ee6d2b
Sannidhi Shetty <sannidhishetty9@gmail.com>
2023-10-03 13:45:58 -0400

    Changed method parameters of Country model to follow coding conventions.

--- Risk/src/main/java/Models/Country.java
+++ Risk/src/main/java/Models/Country.java
@@ -14,11 +14,11 @@ public class Country {
 		
 	}
 	
-	public Country(int d_countryId, String d_countryName, int d_continentId,ArrayList<Integer> d_neighbours) {
-		this.d_countryId = d_countryId;
-		this.d_countryName = d_countryName;
-		this.d_continentId = d_continentId;
-		this.d_neighbours=d_neighbours;
+	public Country(int p_countryId, String p_countryName, int p_continentId,ArrayList<Integer> p_neighbours) {
+		this.d_countryId = p_countryId;
+		this.d_countryName = p_countryName;
+		this.d_continentId = p_continentId;
+		this.d_neighbours=p_neighbours;
 	}
 
 	public ArrayList<Integer> getD_neighbours() {
@@ -27,8 +27,8 @@ public class Country {
 	}
 
 
-	public void setD_neighbours(int l_countryId,HashMap<Integer,ArrayList<Integer>> l_neighbouringCountries) {
-		this.d_neighbours=l_neighbouringCountries.get(l_countryId);
+	public void setD_neighbours(int p_countryId,HashMap<Integer,ArrayList<Integer>> p_neighbouringCountries) {
+		this.d_neighbours=p_neighbouringCountries.get(p_countryId);
 	}
 
 	public int getD_countryId() {
@@ -39,20 +39,20 @@ public class Country {
 		return d_continentId;
 	}
 
-	public void setD_continentId(int d_continentId) {
-		this.d_continentId = d_continentId;
+	public void setD_continentId(int p_continentId) {
+		this.d_continentId = p_continentId;
 	}
 
-	public void setD_countryId(int d_countryId) {
-		this.d_countryId = d_countryId;
+	public void setD_countryId(int p_countryId) {
+		this.d_countryId = p_countryId;
 	}
 
 	public String getD_countryName() {
 		return d_countryName;
 	}
 
-	public void setD_countryName(String d_countryName) {
-		this.d_countryName = d_countryName;
+	public void setD_countryName(String p_countryName) {
+		this.d_countryName = p_countryName;
 	}
 	public String get_nameFromId(ArrayList<Country> p_countryList,int p_countryId)  
 	{
