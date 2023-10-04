c0b95c431b6472f5a228fb93a9b3ccd92f2af168
daiviksaharan <daiviksaharan@daiviks-air.home>
2023-10-04 14:23:17 -0400

    Made changes in junitSuite

--- Risk/src/test/java/Tools/JunitTestSuite.java
+++ Risk/src/test/java/Tools/JunitTestSuite.java
@@ -3,8 +3,7 @@ import org.junit.platform.suite.api.*;
 @org.junit.platform.suite.api.Suite
 @SelectClasses(
 		{MapEditorTest.class,
-PlayersGameplayTest.class,
-SaveMapTest.class
+PlayersGameplayTest.class
 })
 public class JunitTestSuite {
 } 
