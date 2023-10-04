ac2198a194a5a2b890ee2cf7f3a8d457dcb7b47a
Pruthviraj-Horadi <pruthviraj.h.2000@gmail.com>
2023-10-03 23:05:53 -0400

    Created Junit Test Suit for all Tests

---
+++ Risk/src/test/java/Tools/JunitTestSuite.java
@@ -0,0 +1,10 @@
+package Tools;
+import org.junit.platform.suite.api.*;
+@org.junit.platform.suite.api.Suite
+@SelectClasses(
+		{MapEditorTest.class,
+PlayersGameplayTest.class,
+})
+public class JunitTestSuite {
+} 
+
