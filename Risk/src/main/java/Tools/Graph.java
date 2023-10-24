b8fece28f97810546dbda99aca8bd95488ef11d6
daiviksaharan <daiviksaharan@daiviks-air.home>
2023-10-23 21:46:03 -0400

    Created graph class to check the validity of map

---
+++ Risk/src/main/java/Tools/Graph.java
@@ -0,0 +1,29 @@
+package Tools;
+
+import java.util.LinkedList;
+
+public class Graph extends countriesAsGraph{
+	 
+	
+	public Graph(int d_noOfCountries,Connectivity p_connectivity)
+	{
+		this.d_noOfCountries=d_noOfCountries;
+		this.d_adjacentCountriesList=new LinkedList[d_noOfCountries+1];
+		for(int i=1;i<=d_noOfCountries;i++)
+		{
+			d_adjacentCountriesList[i]=new LinkedList();
+		}
+		for(int i=0;i<p_connectivity.getD_countryList().size();i++)
+		{
+			for(int j=0;j<p_connectivity.getD_countryList().get(i).getD_neighbours().size();j++)
+				this.addCountries(p_connectivity.getD_countryList().get(i).getD_countryId(), p_connectivity.getD_countryList().get(i).getD_neighbours().get(j));
+		}
+
+	}
+	public void addCountries(int d_noOfCountries,int p_countryId)
+	{
+		d_adjacentCountriesList[d_noOfCountries].add(p_countryId);
+		
+	}
+	
+}
