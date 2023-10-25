1de16069a1172057fbf56cabd842c2ba5270e7a2
Raghav Dalmia <raghudalmia1712@gmail.com>
2023-10-25 16:19:49 -0400

    added logger to MapCreator

--- Risk/src/main/java/Tools/MapCreater.java
+++ Risk/src/main/java/Tools/MapCreater.java
@@ -3,6 +3,8 @@ package Tools;
 import java.io.File;
 import java.io.IOException;
 
+import Models.LogEntryBuffer;
+
 /**
  * The class MapCreator will write in the file whenever user creates a map.
  *
@@ -20,16 +22,21 @@ public class MapCreater {
 
 	public static void createMap(String p_fileName,String p_pathName) 
 	{
+		LogEntryBuffer d_logEntryBuffer = new LogEntryBuffer();
 		
 		File directory = new File(p_pathName);
 		File f = new File(directory,p_fileName+".map");
 		boolean success;
 		try {
 			success = f.createNewFile();
-			if(success) 
+			if(success) {
+				d_logEntryBuffer.log("File Created successfully");
 				System.out.println("File Created successfully");
-			else
+			}
+			else {
+				d_logEntryBuffer.log("Unable to create new file");
 				System.out.println("Unable to create new file");
+			}
 			
 		} catch (IOException e) {
 			System.out.println("Unable to create a new file");
