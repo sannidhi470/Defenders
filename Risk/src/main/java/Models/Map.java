ac4cd5e3267d545402b1e069ff93ab421ecd8fa3
daiviksaharan <daiviksaharan@daiviks-air.wireless.concordia.ca>
2023-11-23 21:34:05 -0500

    Created checkMap function in map.java

--- Risk/src/main/java/Models/Map.java
+++ Risk/src/main/java/Models/Map.java
@@ -1,5 +1,14 @@
 package Models;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Tools.Connectivity;
+import Tools.ConquestMapLoader;
+import Tools.MapCheck;
+import Tools.MapLoader;
+
 /**
  * The class Map defines Maps and it's properties such as File Name and checker if the file exists or not.
  *
@@ -58,6 +67,41 @@ public class Map {
 
 	public void setD_mapExist(boolean p_mapExist) {
 		this.d_mapExist = p_mapExist;
+	}
+	
+	public static void checkMap(Connectivity p_connectivity,String p_mapName)
+	{
+		String l_fileName = p_mapName;
+		String l_copyFileName=l_fileName;
+		Map l_map=new Map();
+		File f = new File("");
+		p_connectivity.setD_mapName(p_mapName);
+		String absolute = f.getAbsolutePath();
+        p_connectivity.setD_pathName(absolute+ File.separator+"src/main/resources");
+        l_fileName = absolute + File.separator + "src/main/resources" + File.separator + l_fileName+".map";
+        p_connectivity.setD_FilepathName(l_fileName);
+        l_map.setD_mapFileName(l_fileName);
+        ArrayList<String> l_fileContent=new ArrayList<String>();
+        if(MapCheck.validateMap(l_copyFileName,p_connectivity.getD_pathName()))
+        {
+       	 try 
+       	 {
+       	   		Scanner l_input = new Scanner(new File(l_fileName));
+       	   		
+       	   		while(l_input.hasNextLine())
+       	   		{
+       	   			l_fileContent.add(l_input.nextLine());
+       	   		}
+       	 }catch(Exception e)
+       	 {
+       		 System.out.println("File not found exception");
+       	 }
+        }
+        if(l_fileContent.contains("[Territories]"))
+        	ConquestMapLoader.loadMap(p_connectivity, p_mapName);
+        if(l_fileContent.contains("[countries]"))
+        	MapLoader.loadMap(p_connectivity, p_mapName);
+        		
 	}
 	
 	
